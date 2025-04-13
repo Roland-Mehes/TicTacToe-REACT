85bd87c937b0ceb64eb283c344e331a796f145a6
Roland <mehesroli@gmail.com>
2025-04-13 17:16:10 +0300

    adding game header with SVG -s , still no functionality on restart button, only design

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/register/Register';
 import GameMenu from './pages/gameMenu/GameMenu';
+import Game from './pages/game/game';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route index element={<Register />} />
           <Route path="gameMenu" element={<GameMenu />} />
+          <Route path="TicTacToe" element={<Game />} />
         </Routes>
       </Router>
     </>
--- src/pages/game/game.jsx
+++ src/pages/game/game.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import styles from '/.game.module.css';
+import styles from './game.module.css';
+import Board from './components/Board';
+import GameHeader from './components/gameHeader';
 
-const game = () => {
-  return <div className={styles.mainContainer}>game</div>;
+const Game = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.header}>
+        <GameHeader></GameHeader>
+      </div>
+      <Board></Board>
+      <h3>Footer</h3>
+    </div>
+  );
 };
 
-export default game;
+export default Game;
